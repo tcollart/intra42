de5506b585f40ad5aee72a23cca3ba2aca45350f
Thomas COLLART <tcollart@e3r8p19.42.fr>
2015-02-09 21:04:18 +0100

    Add tests for anonymous user

--- framework1/app/tests.py
+++ framework1/app/tests.py
@@ -9,6 +9,54 @@ class Framework1TestCase(TestCase):
         User.objects.create_superuser(username='admin', email=None, password='admin')
         self.client = Client()
 
+    def test_anonymous_user_connection(self):
+        """
+        Anonymous user:
+            - Access sign out page with an AnonymousUser
+            - Access sign in page logging in User
+            - Access sign up page while logged in
+            - Access admin page as simple user
+            - Access sign in page while logged in
+            - Delete an user
+        """
+
+        response = self.client.get('/signout/')
+        self.assertRedirects(response, '/', status_code=302, target_status_code=200, msg_prefix='')
+
+        response = self.client.login(username='fake', password='fake')
+        self.assertEqual(response, False)
+
+        response = self.client.get('/signup/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/signin/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/admin/')
+        self.assertRedirects(response, '/admin/user/', status_code=301, target_status_code=302, msg_prefix='')
+
+        response = self.client.get('/admin/user/')
+        self.assertRedirects(response, '/', status_code=302, target_status_code=200, msg_prefix='')
+
+        response = self.client.get('/admin/user/create/')
+        self.assertRedirects(response, '/', status_code=302, target_status_code=200, msg_prefix='')
+
+        response = self.client.get('/admin/user/1/')
+        self.assertRedirects(response, '/', status_code=302, target_status_code=200, msg_prefix='')
+
+        response = self.client.get('/admin/user/delete/1/')
+        self.assertRedirects(response, '/', status_code=302, target_status_code=200, msg_prefix='')
+
+        response = self.client.post('/admin/user/delete/1/', {'pk': '1'})
+        self.assertRedirects(response, '/', status_code=302, target_status_code=200, msg_prefix='')
+
+        user = User.objects.filter(pk=1)
+        self.assertEqual(len(user), 1)
+
+        response = self.client.get('/signout/')
+        self.assertRedirects(response, '/', status_code=302, target_status_code=200, msg_prefix='')
+
+
     def test_simple_user_connection(self):
         """
         Simple user:
